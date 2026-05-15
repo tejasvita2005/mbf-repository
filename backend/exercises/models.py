a99e06fc7a20cf44dd6278dae59232a5d6316fad
Vinaykumar1008 <v6744251@gmail.com>
2026-05-15 21:15:20 +0530

    Fixed merge conflicts

--- backend/exercises/models.py
+++ backend/exercises/models.py
@@ -1,5 +1,5 @@
-<<<<<<< HEAD
 from django.db import models
+
 
 class Exercise(models.Model):
     name = models.CharField(max_length=100)
@@ -13,20 +13,4 @@ class Exercise(models.Model):
     description = models.TextField(blank=True, null=True)
 
     def __str__(self):
-=======
-from django.db import models
-
-class Exercise(models.Model):
-    name = models.CharField(max_length=100)
-    injury_type = models.CharField(max_length=100)
-    fitness_level = models.CharField(max_length=50)
-    equipment = models.CharField(max_length=100)
-    difficulty = models.CharField(max_length=50)
-    body_part = models.CharField(max_length=100)
-    reps = models.IntegerField()
-    sets = models.IntegerField()
-    description = models.TextField(blank=True, null=True)
-
-    def __str__(self):
->>>>>>> bae68d7b901b6fef0c95bf2bddf48472023e5e96
         return self.name
--- backend/exercises/serializers.py
+++ backend/exercises/serializers.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from rest_framework import serializers
 from .models import Exercise
 
@@ -7,14 +6,4 @@ class ExerciseSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Exercise
-=======
-from rest_framework import serializers
-from .models import Exercise
-
-
-class ExerciseSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Exercise
->>>>>>> bae68d7b901b6fef0c95bf2bddf48472023e5e96
         fields = '__all__'
