b4ddbd7df966226089c595e7d55935bde05a254c
Vinaykumar1008 <v6744251@gmail.com>
2026-05-15 22:16:52 +0530

    added

--- backend/exercises/config/asgi.py
+++ backend/exercises/config/asgi.py
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
 """
 ASGI config for config project.
 
-It exposes the ASGI callable as a module-level variable named ``application``.
+It exposes the ASGI callable as a module-level variable named `application`.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
@@ -15,21 +14,3 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_asgi_application()
-=======
-"""
-ASGI config for config project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
-"""
-
-import os
-
-from django.core.asgi import get_asgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-
-application = get_asgi_application()
->>>>>>> bae68d7b901b6fef0c95bf2bddf48472023e5e96
